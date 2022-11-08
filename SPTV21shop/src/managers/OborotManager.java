0b772acc0b07e3584e8358618c137f5c1a9475b6
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2022-11-08 13:22:13 +0400

    Add files via upload

---
+++ SPTV21shop/src/managers/OborotManager.java
@@ -0,0 +1,35 @@
+package managers;
+
+import enyity.Oborot;
+import enyity.Product;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class OborotManager {
+ private final Scanner scanner;
+    public OborotManager() {
+        scanner = new Scanner(System.in);  
+    }
+    
+   public Oborot[] changeOborot(Oborot[] oborots) {
+        int numOborotForEdit = scanner.nextInt(); scanner.nextLine();
+    System.out.print("Введите нынешний оборот магазина: ");
+            oborots[numOborotForEdit - 1].setSumma(scanner.nextInt());scanner.nextLine();
+     return oborots; 
+}
+ public void printListOborots(Oborot[] oborots){
+        for (int i = 0; i < oborots.length; i++) {
+            System.out.println(i+1+". "+oborots[i].getSumma());
+        }
+ 
+        
+}}
